258ac581ff43ef992a9d94655ee75cfafefbba70
AXEL LARESE <axelarese@gmail.com>
2023-09-27 14:58:14 +0200

    Push début après-midi du 27 septembre : la fonctionnalité ToDoListe fonctionne & les datas sont bien stockées dans le navigateur

--- script.js
+++ script.js
@@ -14,15 +14,28 @@ function addTasks () {
         span.innerHTML="\u00d7";
         li.appendChild(span);
     }
-inputText.value = "";
+    inputText.value = "";
+    saveData ();
 }
 
 list.addEventListener("click", function (e) {
-    if (e.target.tagName === "li") {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData ();
     }
     
-    else if (e.target.tagName === "span") {
+    else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData ();
     }
 }, false);
+
+function saveData () {
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function historyTasks () {
+    list.innerHTML = localStorage.getItem("data");
+}
+
+historyTasks();
