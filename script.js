1a12dd711a8d51c0c15156b56878bd34574744db
techmefr <compigni.gaetan@gmail.com>
2023-10-04 14:57:20 +0200

    css for the main content

--- script.js
+++ script.js
@@ -1,75 +1,76 @@
 // Fonctions JS ToDo List
 
-document.addEventListener("DOMContentLoaded", function() {
-    const taskInput = document.getElementById("task");
-    const addTaskButton = document.getElementById("addTask");
-    const taskCategory = document.getElementById("taskCategory");
+document.addEventListener("DOMContentLoaded", function () {
+  const taskInput = document.getElementById("task");
+  const addTaskButton = document.getElementById("addTask");
+  const taskCategory = document.getElementById("taskCategory");
 
-    addTaskButton.addEventListener("click", function() {
-        const taskText = taskInput.value;
-        const category = taskCategory.value.toLowerCase();
-        if (taskText.trim() === "" || category === "") return;
+  addTaskButton.addEventListener("click", function () {
+    const taskText = taskInput.value;
+    const category = taskCategory.value.toLowerCase();
+    if (taskText.trim() === "" || category === "") return;
 
-        const taskItem = document.createElement("li");
-        taskItem.textContent = taskText;
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskText;
 
-        const taskDiv = document.createElement("div");
-        taskDiv.appendChild(taskItem);
+    const taskDiv = document.createElement("div");
+    taskDiv.appendChild(taskItem);
 
-        const customFolderName = category.trim();
-        const customFolderList = document.getElementById(customFolderName + "Tasks");
-        if (customFolderList) {
-            const taskListDiv = customFolderList.querySelector("ul");
-            taskListDiv.appendChild(taskDiv);
-        } else {
-            alert("Le dossier personnalisé n'existe pas.");
-        }
+    const customFolderName = category.trim();
+    const customFolderList = document.getElementById(
+      customFolderName + "Tasks"
+    );
+    if (customFolderList) {
+      const taskListDiv = customFolderList.querySelector("ul");
+      taskListDiv.appendChild(taskDiv);
+    } else {
+      alert("Le dossier personnalisé n'existe pas.");
+    }
 
-        taskInput.value = "";
-    });
+    taskInput.value = "";
+  });
 
-    const newFolderNameInput = document.getElementById("newFolderName");
-    const createFolderButton = document.getElementById("createFolder");
-    const selectElement = document.getElementById("taskCategory");
+  const newFolderNameInput = document.getElementById("newFolderName");
+  const createFolderButton = document.getElementById("createFolder");
+  const selectElement = document.getElementById("taskCategory");
 
-    createFolderButton.addEventListener("click", function() {
-        const folderName = newFolderNameInput.value.trim().toLowerCase();
-        if (folderName === "") return;
+  createFolderButton.addEventListener("click", function () {
+    const folderName = newFolderNameInput.value.trim().toLowerCase();
+    if (folderName === "") return;
 
-        const newTaskListDiv = document.createElement("div");
-        newTaskListDiv.classList.add("task-list");
-        newTaskListDiv.id = folderName + "Tasks";
+    const newTaskListDiv = document.createElement("div");
+    newTaskListDiv.classList.add("task-list");
+    newTaskListDiv.id = folderName + "Tasks";
 
-        const folderTitle = document.createElement("h2");
-        folderTitle.textContent = newFolderNameInput.value.trim();
+    const folderTitle = document.createElement("h2");
+    folderTitle.textContent = newFolderNameInput.value.trim();
 
-        const folderTaskList = document.createElement("ul");
+    const folderTaskList = document.createElement("ul");
 
-        newTaskListDiv.appendChild(folderTitle);
-        newTaskListDiv.appendChild(folderTaskList);
+    newTaskListDiv.appendChild(folderTitle);
+    newTaskListDiv.appendChild(folderTaskList);
 
-        document.querySelector(".todo-app").appendChild(newTaskListDiv);
+    document.querySelector(".todo-app").appendChild(newTaskListDiv);
 
-        newFolderNameInput.value = "";
+    newFolderNameInput.value = "";
 
-        const option = document.createElement("option");
-        option.value = folderName;
-        option.textContent = folderTitle.textContent;
-        selectElement.appendChild(option);
-    });
+    const option = document.createElement("option");
+    option.value = folderName;
+    option.textContent = folderTitle.textContent;
+    selectElement.appendChild(option);
+  });
 });
 
 // SAVES
 
-function saveData () {
-    localStorage.setItem("data", list.innerHTML);
+function saveData() {
+  localStorage.setItem("data", list.innerHTML);
 }
-function historyTasks () {
-    list.innerHTML = localStorage.getItem("data");
+function historyTasks() {
+  list.innerHTML = localStorage.getItem("data");
 }
 historyTasks();
 
 function myFunction() {
-    alert("Votre message a bien été envoyé");
+  alert("Votre message a bien été envoyé");
 }
-
