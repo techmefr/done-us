b8423987f351c1082d951eed7df53e4763d5353b
AXEL LARESE <axelarese@gmail.com>
2023-10-04 16:16:33 +0200

    Travail de l'après midi du 4 octobre 2023

--- script.js
+++ script.js
@@ -13,22 +13,43 @@ document.addEventListener("DOMContentLoaded", function() {
         const taskItem = document.createElement("li");
         taskItem.textContent = taskText;
 
+        // Début de la fonction pour supprimer les tâches
+
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        taskItem.appendChild(span);
+
+        span.addEventListener ("click", function (e) {
+            if (e.target.tagName === "SPAN") {
+                e.target.parentElement.remove();
+                saveData();
+            }
+            saveData ()
+        }, false);
+
+        // Fin de la fonction pour supprimer les tâches
+
         const taskDiv = document.createElement("div");
-        taskDiv.appendChild(taskItem);
+        taskDiv.appendChild(taskItem);  
 
         const customFolderName = category.trim();
         const customFolderList = document.getElementById(customFolderName + "Tasks");
         if (customFolderList) {
             const taskListDiv = customFolderList.querySelector("ul");
             taskListDiv.appendChild(taskDiv);
+            taskInput.value = "";
+            saveData ();
         } else {
             alert("Le dossier personnalisé n'existe pas.");
+            saveData ();
         }
 
         taskInput.value = "";
         saveData ();
     });
 
+    
+        
     const newFolderNameInput = document.getElementById("newFolderName");
     const createFolderButton = document.getElementById("createFolder");
     const selectElement = document.getElementById("taskCategory");
@@ -41,10 +62,26 @@ document.addEventListener("DOMContentLoaded", function() {
         newTaskListDiv.classList.add("task-list");
         newTaskListDiv.id = folderName + "Tasks";
 
+        // Début de la fonction pour supprimer les catégories
+
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        newTaskListDiv.appendChild(span);
+
+        span.addEventListener ("click", function (e) {
+            if (e.target.tagName === "SPAN") {
+                e.target.parentElement.remove();
+                saveData();
+            }
+        }, false);
+
+         // Fin de la fonction pour supprimer les catégories
+
         const folderTitle = document.createElement("h2");
         folderTitle.textContent = newFolderNameInput.value.trim();
 
         const folderTaskList = document.createElement("ul");
+        
 
         newTaskListDiv.appendChild(folderTitle);
         newTaskListDiv.appendChild(folderTaskList);
@@ -57,6 +94,7 @@ document.addEventListener("DOMContentLoaded", function() {
         option.value = folderName;
         option.textContent = folderTitle.textContent;
         selectElement.appendChild(option);
+
     });
     saveData ();
 });
